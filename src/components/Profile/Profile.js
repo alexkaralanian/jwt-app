976b3aec8bb4f5587d245044bc1f5571fb51dc43
Alex Karalanian <alex.karalanian@gmail.com>
2017-09-21 23:46:48 -0400

    styling changes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,29 +2,32 @@ import PropTypes from "prop-types";
 import React from "react";
 import { Redirect } from "react-router-dom";
 import "./styles.css";
-import profilePic from "./img/img-1.jpg"
+import profilePic from "./img/img-1.jpg";
+
 const Profile = ({ isAuthed, user, logout }) =>
-  !isAuthed
-    ? <Redirect
-        to={{
-          pathname: "/login"
-        }}
-      />
-    : <div className="container">
-        <div className ="centeredContainer">
-          <h3 className="userName">
-            Hello {user.firstName} {user.lastName}!
-          </h3>
-          <img
-            className="profilePic"
-            alt="user profile pic"
-            src={user.photo ? user.photo : profilePic }
-          />
-          <button className="logout btn btn-primary" onClick={logout}>
-            LOGOUT
-          </button>
+  !isAuthed ? (
+    <Redirect
+      to={{
+        pathname: "/login"
+      }}
+    />
+  ) : (
+    <div className="container">
+      <div className="centeredContainer">
+        <div className="userNameContainer">
+          <h3 className="userName">Hello, {user.firstName}!</h3>
         </div>
-      </div>;
+        <img
+          className="profilePic"
+          alt="user profile pic"
+          src={user.photo ? user.photo : profilePic}
+        />
+        <button className="logout btn btn-primary" onClick={logout}>
+          <span id="logoutText">LOGOUT</span>
+        </button>
+      </div>
+    </div>
+  );
 
 Profile.defaultProps = {
   isAuthed: false,
--- start-client.js
+++
@@ -1,5 +0,0 @@
-const args = ["start-client"];
-const opts = { stdio: "inherit", shell: true };
-require("child_process").spawn("npm", args, opts);
-
-//cwd: "../client",
